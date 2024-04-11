fcffb31e16ce63cd33409ddc570dc123a0e24900
maxime-jcnl <112609114+maxime-jcnl@users.noreply.github.com>
2024-04-11 16:15:20 +0200

    v2 coordonnée

--- script.js
+++ script.js
@@ -1,137 +1,116 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("search-input").value = ""; // Efface le contenu de la barre de recherche
-
-  var maCarte = L.map("maCarte", {
-    zoomControl: false,
-    scrollWheelZoom: false,
-    smoothWheelZoom: true,
-    smoothSensitivity: 5,
-  }).setView([48.8566, 2.3522], 13);
-
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; Contributeurs d'OpenStreetMap",
-  }).addTo(maCarte);
-
-  var currentMarker = null;
-
-  // Fonction pour exécuter la recherche et placer un marqueur
-  function executeSearch(lat, lon) {
-    if (currentMarker) {
-      maCarte.removeLayer(currentMarker);
+    document.getElementById("search-input").value = ""; // Efface le contenu de la barre de recherche
+  
+    var maCarte = L.map("maCarte", {
+      zoomControl: false,
+      scrollWheelZoom: false,
+      smoothWheelZoom: true,
+      smoothSensitivity: 5,
+    }).setView([48.8566, 2.3522], 13);
+  
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; Contributeurs d'OpenStreetMap",
+    }).addTo(maCarte);
+  
+    var currentMarker = null;
+  
+    // Fonction pour exécuter la recherche et placer un marqueur
+    function executeSearch(lat, lon) {
+      if (currentMarker) {
+        maCarte.removeLayer(currentMarker);
+      }
+      maCarte.setView([lat, lon], 13);
+      currentMarker = L.marker([lat, lon]).addTo(maCarte);
     }
-    maCarte.setView([lat, lon], 13);
-    currentMarker = L.marker([lat, lon]).addTo(maCarte);
-  }
-
-  document
-    .getElementById("search-button")
-    .addEventListener("click", function () {
-      var query = document.getElementById("search-input").value;
-      fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${query}`)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.length > 0) {
-            var firstResult = data[0];
-            executeSearch(firstResult.lat, firstResult.lon);
-          } else {
-            // Vérifier si l'utilisateur a entré des coordonnées GPS
-            var coordinates = extractCoordinates(query);
-            if (coordinates) {
-              executeSearch(coordinates.lat, coordinates.lon);
-            } else {
-              alert("Lieu non trouvé. Veuillez essayer une autre recherche.");
-            }
-          }
-        })
-        .catch((error) => console.log(error));
-    });
-
-  document
-    .getElementById("search-input")
-    .addEventListener("input", function (e) {
-      var userInput = e.target.value;
-      if (userInput.length > 2) {
-        fetch(`https://photon.komoot.io/api/?q=${userInput}`)
+  
+    document
+      .getElementById("search-button")
+      .addEventListener("click", function () {
+        var query = document.getElementById("search-input").value;
+        fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${query}`)
           .then((response) => response.json())
           .then((data) => {
-            console.log(data); // Pour inspecter les données dans la console
-
-            // Filtrer les résultats pour ne conserver que ceux situés en France
-            var franceResults = data.features.filter((item) => {
-              // Vérifier si les coordonnées se trouvent en France (approximativement)
-              return (
-                item.geometry.coordinates[0] > -5.0 &&
-                item.geometry.coordinates[0] < 10.0 &&
-                item.geometry.coordinates[1] > 41.0 &&
-                item.geometry.coordinates[1] < 51.0
-              );
-            });
-
-            document.getElementById("suggestions-container").innerHTML = "";
-            franceResults.forEach((item) => {
-              var suggestionItem = document.createElement("div");
-              suggestionItem.textContent = item.properties.name;
-              suggestionItem.classList.add("suggestion-item");
-              suggestionItem.addEventListener("click", () => {
-                document.getElementById("suggestions-container").style.display =
-                  "none";
-                executeSearch(
-                  item.geometry.coordinates[1],
-                  item.geometry.coordinates[0]
+            if (data.length > 0) {
+              var firstResult = data[0];
+              executeSearch(firstResult.lat, firstResult.lon);
+            } else {
+              // Vérifier si l'utilisateur a entré des coordonnées GPS
+              var coordinates = query.split(",");
+              if (coordinates.length === 2 && !isNaN(coordinates[0]) && !isNaN(coordinates[1])) {
+                executeSearch(parseFloat(coordinates[1]), parseFloat(coordinates[0]));
+              } else {
+                alert("Lieu non trouvé. Veuillez essayer une autre recherche.");
+              }
+            }
+          })
+          .catch((error) => console.log(error));
+      });
+  
+    document
+      .getElementById("search-input")
+      .addEventListener("input", function (e) {
+        var userInput = e.target.value;
+        if (userInput.length > 2) {
+          fetch(`https://photon.komoot.io/api/?q=${userInput}`)
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data); // Pour inspecter les données dans la console
+  
+              // Filtrer les résultats pour ne conserver que ceux situés en France
+              var franceResults = data.features.filter((item) => {
+                // Vérifier si les coordonnées se trouvent en France (approximativement)
+                return (
+                  item.geometry.coordinates[0] > -5.0 &&
+                  item.geometry.coordinates[0] < 10.0 &&
+                  item.geometry.coordinates[1] > 41.0 &&
+                  item.geometry.coordinates[1] < 51.0
                 );
               });
-              document
-                .getElementById("suggestions-container")
-                .appendChild(suggestionItem);
-            });
-
-            // Vérifier si l'utilisateur a entré des coordonnées GPS
-            var coordinates = extractCoordinates(userInput);
-            if (coordinates) {
-              var suggestionItem = document.createElement("div");
-              suggestionItem.textContent = `Coordonnées GPS : ${coordinates.lat}, ${coordinates.lon}`;
-              suggestionItem.classList.add("suggestion-item");
-              suggestionItem.addEventListener("click", () => {
-                document.getElementById("suggestions-container").style.display =
-                  "none";
-                executeSearch(coordinates.lat, coordinates.lon);
+  
+              document.getElementById("suggestions-container").innerHTML = "";
+              franceResults.forEach((item) => {
+                var suggestionItem = document.createElement("div");
+                suggestionItem.textContent = item.properties.name;
+                suggestionItem.classList.add("suggestion-item");
+                suggestionItem.addEventListener("click", () => {
+                  document.getElementById("suggestions-container").style.display =
+                    "none";
+                  executeSearch(
+                    item.geometry.coordinates[1],
+                    item.geometry.coordinates[0]
+                  );
+                });
+                document
+                  .getElementById("suggestions-container")
+                  .appendChild(suggestionItem);
               });
-              document
-                .getElementById("suggestions-container")
-                .appendChild(suggestionItem);
-            }
-
-            document.getElementById("suggestions-container").style.display =
-              "block";
-          })
-          .catch((error) =>
-            console.log(
-              "Erreur lors de la récupération des suggestions:",
-              error
-            )
-          );
-      }
-    });
-
-  // Fonction pour extraire les coordonnées à partir d'une chaîne de caractères
-  function extractCoordinates(input) {
-    var regex = /(?:Lat|latitude|Latitud|Lon|longitude|Longitud)\s*:\s*(-?\d+\.\d+)\s*,\s*(-?\d+\.\d+)/i;
-    var match = input.match(regex);
-    if (match && match.length === 3) {
-      return {
-        lat: parseFloat(match[1]),
-        lon: parseFloat(match[2]),
-      };
-    } else {
-      var coordinates = input.split(/[ ,]/);
-      if (coordinates.length === 2 && !isNaN(coordinates[0]) && !isNaN(coordinates[1])) {
-        return {
-          lat: parseFloat(coordinates[0]),
-          lon: parseFloat(coordinates[1]),
-        };
-      } else {
-        return null;
-      }
-    }
-  }
-});
+  
+              // Vérifier si l'utilisateur a entré des coordonnées GPS
+              var coordinates = userInput.split(",");
+              if (coordinates.length === 2 && !isNaN(coordinates[0]) && !isNaN(coordinates[1])) {
+                var suggestionItem = document.createElement("div");
+                suggestionItem.textContent = `Coordonnées GPS : ${coordinates[1]}, ${coordinates[0]}`;
+                suggestionItem.classList.add("suggestion-item");
+                suggestionItem.addEventListener("click", () => {
+                  document.getElementById("suggestions-container").style.display =
+                    "none";
+                  executeSearch(parseFloat(coordinates[1]), parseFloat(coordinates[0]));
+                });
+                document
+                  .getElementById("suggestions-container")
+                  .appendChild(suggestionItem);
+              }
+  
+              document.getElementById("suggestions-container").style.display =
+                "block";
+            })
+            .catch((error) =>
+              console.log(
+                "Erreur lors de la récupération des suggestions:",
+                error
+              )
+            );
+        }
+      });
+  });
+  
